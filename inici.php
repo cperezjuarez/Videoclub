aa85cf174065260b4212ca61694b8158d0e03112
cperezjuarez <cperezjuarez@cifpfbmoll.eu>
2025-10-16 23:37:31 +0200

    Clase Client en proceso

--- inici.php
+++ inici.php
@@ -1,1 +1,10 @@
 <?php
+include_once("./src/clases/Client.php");
+
+//------------- PRUEBAS DE LA CLASE CLIENT.PHP -------------
+
+$client = new Client("Sefirot", 123, 5);
+$client2 = new Client("Cloud", 456);
+
+echo $client->mostraResum();
+echo $client2->mostraResum();
--- src/clases/Client.php
+++ src/clases/Client.php
@@ -1,15 +1,84 @@
 <?php
-class client {
+include_once("./Soport.php");
+
+class Client {
     // Atributos
     public String $nom;
     private int $numero;
+    private array $soportsLlogats = [];
+    private int $numSoportsLlogats;
     private int $maxLloguerConcurrent;
 
     // Métodos
-    public function __construct($nom, $numero, $maxLloguerConcurrent = 3) {
-
+    public function __construct(String $nom, int $numero, int $maxLloguerConcurrent = 3) {
+        $this->nom = $nom;
+        $this->setNumero($numero);
+        $this->maxLloguerConcurrent = $maxLloguerConcurrent;
     }
 
-    // Getters & Setters
+    public function mostraResum(): String {
+        return "$this->nom Té actius " . count($this->soportsLlogats) . " lloguers actualment. </br>";
+    }
 
+    public function teLlogat(Soport $s): bool {
+        $llogat = false; // Variable que devolveremos para saber si el soporte esta alquilado o no.
+
+        // Por cada soporte de la lista, comprovamos si es el que nos pasan por parámetro
+        forEach($this->soportsLlogats as $soport) {
+            if ($soport->titol == $s->titol) {
+                $llogat = true; // Si lo encontramos cambiamos la variable a true
+            }
+        }
+
+        return $llogat;
+    }
+
+    public function llogar(Soport $s): bool {
+        $llogable = false; // Variable para saber si se alquilará o no
+
+        // Mirmos que el soporte no esté ya alquilado
+        $llogat = self::teLlogat($s);
+        if($llogat) {echo "Llogat correctament: $s->titol a client: $this->nom";} // Mensaje
+
+        // Miramos que no se haya superado la quota de alquiler
+        $quotaSuperada = true;
+
+        if (count($this->soportsLlogats) < $this->maxLloguerConcurrent) {
+            $quotaSuperada = false;
+        } else {
+            echo "Llogat correctament: $s->titol a client: $this->nom"; // Mensaje
+        }
+
+        // Comprobación final
+        if (!$llogat && !$quotaSuperada) {
+            $llogable = true;
+        }
+
+        // Si es alquilable, procedemos ello:
+        if ($llogable) {
+            $this->numSoportsLlogats++; // Aumentamos el número de soportes alquilados
+            array_push($this->soportsLlogats, $s); // Lo añadimos a la lista de alquilados
+            echo "Llogat correctament: $s->titol a client: $this->nom"; // Mensaje
+        }
+
+        return $llogable;
+    }
+
+    //public function tornar(int $numSport): bool {}
+
+    // Getters & Setters
+    public function getNumero(): int
+    {
+        return $this->numero;
+    }
+
+    public function setNumero(int $numero): void
+    {
+        $this->numero = $numero;
+    }
+
+    public function getNumSoportsLlogats(): int
+    {
+        return $this->numSoportsLlogats;
+    }
 }
