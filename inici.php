3a5ccf6411d853a66f5a92ad61479ef6bc6d689b
SvenLee-J <sjansengorreta@cifpfbmoll.eu>
2025-10-18 17:16:36 +0200

    Clase_Soport

--- inici.php
+++ inici.php
@@ -1,1 +1,8 @@
 <?php
+include "Soport.php";
+
+$soport1 = new Soport("Tenet", 22, 3);
+echo "<strong>" . $soport1->titol . "</strong>";
+echo "<br>Preu: " . $soport1->getPreu() . " euros";
+echo "<br>Preu IVA inclos: " . $soport1->getPreuAmbIVA() . " euros";
+$soport1->mostraResum();
--- src/clases/Soport.php
+++ src/clases/Soport.php
@@ -1,1 +1,30 @@
 <?php
+class  Soport
+{
+    public $titol;
+    protected $numero;
+    private $preu;
+    private static $iva = 1.21;
+
+    public function __construct($titol, $numero, $preu){
+        $this->titol = $titol;
+        $this->numero = $numero;
+        $this->preu = $preu;
+    }
+    public function getPreu()
+    {
+        return $this->preu;
+    }
+    public function getPreuAmbIva()
+    {
+        return self::$iva * $this->preu;
+    }
+    public function getNumero()
+    {
+        return $this->numero;
+    }
+    public function mostraResum(){
+        echo "<br>" . "<i>" . $this->titol . "</i>" . "</br>" ;
+        echo $this->preu . " " . "$ " . "(IVA no Inclos)";
+    }
+}
