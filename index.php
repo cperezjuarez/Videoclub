68908dbf16ea1229ac26061230a206aaf5b6ac3d
cperezjuarez <cperezjuarez@cifpfbmoll.eu>
2025-11-03 21:09:37 +0100

    Ejercicio 158 - En proceso

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+<body>
+    <form method="get" action="./pages/login.php">
+        <label for="username">Username</label>
+        <input type="text" name="username">
+
+        <label for="passw">Password</label>
+        <input type="password" name="passw">
+    </form>
+</body>
+</html>
---
+++ pages/login.php
@@ -0,0 +1,14 @@
+<?php
+$username = $_POST["username"];
+$password = $_POST["password"];
+
+$username1 = 'user';
+$password1 = 'user';
+$password2 = 'admin';
+$username2 = 'admin';
+
+if ($username == $username1 || $username == $username2) {
+    if ($password == $password1) {
+
+    }
+}
